54277174332c8a2709478cca8fd80a03ad34db18
Funsho Ayobanjo <funshoayobanjo@gmail.com>
2022-10-04 14:39:06 +0100

    js for hamburger toggle

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
 
+let navOpen = false;
 navToggle.addEventListener("click", () => {
-  nav.classList.toggle("nav_visible");
+  if (!navOpen) {
+    navToggle.classList.add("open");
+    navOpen = true;
+  } else {
+    navToggle.classList.remove("open");
+    navOpen = false;
+  }
 });
+
+// navToggle.addEventListener("click", () => {
+//   nav.classList.toggle("nav_visible");
+// });
