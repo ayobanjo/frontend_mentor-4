a8afb3963648bce9753df8e10b3eb613a2b789a2
Funsho Ayobanjo <funshoayobanjo@gmail.com>
2022-10-04 16:19:51 +0100

    js simplified

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
 
-let navOpen = false;
 navToggle.addEventListener("click", () => {
   nav.classList.toggle("nav_visible");
-
-  if (!navOpen) {
-    navToggle.classList.add("open");
-    navOpen = true;
-  } else {
-    navToggle.classList.remove("open");
-    navOpen = false;
-  }
+  navToggle.classList.toggle("open");
 });
 
+// let navOpen = false;
 // navToggle.addEventListener("click", () => {
 //   nav.classList.toggle("nav_visible");
+
+//   if (!navOpen) {
+//     navToggle.classList.add("open");
+//     navOpen = true;
+//   } else {
+//     navToggle.classList.remove("open");
+//     navOpen = false;
+//   }
 // });
