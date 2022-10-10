aeaf06c688238019b4f84b8800469740539cf9a4
Funsho Ayobanjo <funshoayobanjo@gmail.com>
2022-10-10 17:31:23 +0100

    dropdown working

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
 const menuIcon = document.querySelector(".menu_btn");
 const dropDowns = document.querySelectorAll(".dropdown");
-const dropIcon = document.querySelector(".drop_down-img");
 
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle("menu_icon-change");
   nav.classList.toggle("nav_sidebar");
 });
 
-// const arrdropDown = Array.from(dropDowns);
+// // const arrdropDown = Array.from(dropDowns);
 for (let i = 0; i < dropDowns.length; i++) {
   dropDowns[i].addEventListener("click", function () {
-    dropDowns[i].classList.toggle("drop_down-imgup");
+    const dropIcon = document.querySelectorAll(".drop_down-img");
+    dropIcon[i].classList.toggle("drop_down-imgup");
   });
 }
+
 // navToggle.addEventListener("click", () => {
 //   nav.classList.toggle("nav_visible");
 //   nav.style.color = "white";
